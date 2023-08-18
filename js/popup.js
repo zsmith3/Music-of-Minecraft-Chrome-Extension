e92a6286e6e9e32ce1ed1a5a92751e70b48b1d0a
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2023-08-18 12:24:17 -0600

    fe: song name appears when hovering icon

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,11 @@
 window.onload = function () {
-	chrome.runtime.getBackgroundPage(function (page) {
-		document.getElementById("message").innerText = "Error: " + page.getTracksError;
-	});
+    chrome.runtime.getBackgroundPage(function (page) {
+        var errorMessage = document.getElementById("message");
+        if (page.getTracksError) {
+            errorMessage.innerText = "Error: " + page.getTracksError;
+        } else {
+            var currentSongElement = document.getElementById("current-song");
+            currentSongElement.innerText = "Now Playing: " + page.currentSongName; // Display current song name
+        }
+    });
 };
